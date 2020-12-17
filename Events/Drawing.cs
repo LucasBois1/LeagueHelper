aa96bca356f9a49f14ecc71980dce3093cdbd119
Lucas <lucasjkjk@gmail.com>
2020-12-17 20:51:20 -0300

    Base Commit

---
+++ Events/Drawing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExSharpBase.Modules;
+using ExSharpBase.Game.Objects;
+using SharpDX;
+
+namespace ExSharpBase.Events
+{
+    class Drawing
+    {
+        public static Dictionary<string, bool> DrawingProperties = new Dictionary<string, bool>()
+        {
+            { "DrawRange", true }
+        };
+
+        public static bool IsMenuBeingDrawn = false;
+
+        public static void OnDeviceDraw()
+        {
+            if (Utils.IsGameOnDisplay())
+            {
+                //When ~ key is pressed...
+                DrawMenu();
+
+                if (DrawingProperties["DrawRange"] == true)
+                {
+                    LocalPlayer.DrawAttackRange(Color.GreenYellow, 2.5f);
+
+                    LocalPlayer.DrawAllSpellRange(Color.White);
+                }
+            }
+        }
+
+        private static void DrawMenu()
+        {
+            if (Utils.IsKeyPressed(System.Windows.Forms.Keys.K))
+            {
+                Program.MenuBasePlate.Show();
+                IsMenuBeingDrawn = true;
+            }
+            else
+            {
+                Program.MenuBasePlate.Hide();
+            }
+        }
+    }
+}
